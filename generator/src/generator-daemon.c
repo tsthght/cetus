d5c6255c318f8314dbc7f84007b226d92af28520
tsthght <tsthght@yeah.net>
2018-09-12 14:44:18 +0800

    support keepalive

--- generator/src/generator-daemon.c
+++ generator/src/generator-daemon.c
@@ -1,5 +1,19 @@
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h> /* wait4 */
+#endif
+#ifdef HAVE_SYS_RESOURCE_H
+#include <sys/resource.h> /* getrusage */
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <string.h>
 #include "generator-daemon.h"
 
 void daemonize()
@@ -25,4 +39,106 @@ void daemonize()
         exit(0);
 
     umask(0);
+}
+
+/**
+ * forward the signal to the process group, but not us
+ */
+static void signal_forward(int sig) {
+    signal(sig, SIG_IGN); /* we don't want to create a loop here */
+    kill(0, sig);
+}
+
+gint keepalive(int *child_exit_status, const char *pid_file) {
+    int nprocs = 0;
+    pid_t child_pid = -1;
+
+    /* we ignore SIGINT and SIGTERM and just let it be forwarded to the child instead
+     * as we want to collect its PID before we shutdown too
+     *
+     * the child will have to set its own signal handlers for this
+     */
+
+    for (;;) {
+        /* try to start the children */
+        while (nprocs < 1) {
+            pid_t pid = fork();
+
+            if (pid == 0) {
+                /* child */
+                return 0;
+            } else if (pid < 0) {
+                /* fork() failed */
+                return -1;
+            } else {
+                /* we are the angel, let's see what the child did */
+                /* forward a few signals that are sent to us to the child instead */
+                signal(SIGINT, signal_forward);
+                signal(SIGTERM, signal_forward);
+                signal(SIGHUP, signal_forward);
+                signal(SIGUSR1, signal_forward);
+                signal(SIGUSR2, signal_forward);
+
+                child_pid = pid;
+                nprocs++;
+            }
+        }
+
+        if (child_pid != -1) {
+            struct rusage rusage;
+            int exit_status;
+            pid_t exit_pid;
+
+#ifdef HAVE_WAIT4
+            exit_pid = wait4(child_pid, &exit_status, 0, &rusage);
+#else
+            memset(&rusage, 0, sizeof(rusage)); /* make sure everything is zero'ed out */
+            exit_pid = waitpid(child_pid, &exit_status, 0);
+#endif
+            if (exit_pid == child_pid) {
+
+                /* delete pid file */
+                if (pid_file) {
+                    unlink(pid_file);
+                }
+
+                /* our child returned, let's see how it went */
+                if (WIFEXITED(exit_status)) {
+                    if (child_exit_status) *child_exit_status = WEXITSTATUS(exit_status);
+
+                    return 1;
+                } else if (WIFSIGNALED(exit_status)) {
+                    int time_towait = 2;
+                    /* our child died on a signal
+                     *
+                     * log it and restart */
+
+                    /**
+                     * to make sure we don't loop as fast as we can, sleep a bit between
+                     * restarts
+                     */
+
+                    signal(SIGINT, SIG_DFL);
+                    signal(SIGTERM, SIG_DFL);
+                    signal(SIGHUP, SIG_DFL);
+                    while (time_towait > 0) time_towait = sleep(time_towait);
+
+                    nprocs--;
+                    child_pid = -1;
+                } else if (WIFSTOPPED(exit_status)) {
+                } else {
+                    g_assert_not_reached();
+                }
+            } else if (-1 == exit_pid) {
+                /* EINTR is ok, all others bad */
+                if (EINTR != errno) {
+                    /* how can this happen ? */
+
+                    return -1;
+                }
+            } else {
+                g_assert_not_reached();
+            }
+        }
+    }
 }
--- generator/src/generator.c
+++ generator/src/generator.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "generator-daemon.h"
 
 int main() {
     daemonize();
+    gint child_exit_status = EXIT_SUCCESS;
+    gint ret = keepalive(&child_exit_status, NULL);
+    if (ret > 0) {
+        return 0;
+    } else if (ret < 0) {
+        return 0;
+    } else {
+
+    }
     printf("generator driver\n");
     sleep(1000);
     return 0;
